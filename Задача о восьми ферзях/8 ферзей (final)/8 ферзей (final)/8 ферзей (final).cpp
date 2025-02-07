40e023c109275914f3dda6af3a5bb5888882c18b
Cerinij <stereomadness55@gmail.com>
2025-02-07 10:05:40 +0500

    Add files via upload

---
+++ Задача о восьми ферзях/8 ферзей (final)/8 ферзей (final)/8 ферзей (final).cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+const int n = 8;
+int queens[n];
+int counter = 0;
+
+
+void printSolution()
+{
+    counter++;
+    cout << "Решение " << counter << ":" << endl;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (queens[i] == j)
+            {
+                cout << "Q ";
+            }
+            else
+            {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return;
+}
+
+
+bool check(int row, int col)
+{
+    for (int i = 0; i < row; ++i)
+    {
+        if (queens[i] == col)
+        {
+            return false;
+        }
+        if (abs(queens[i] - col) == abs(i - row))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void searchSolution(int row)
+{
+    if (row == n)
+    {
+        printSolution();
+    }
+    else {
+        for (int col = 0; col < n; ++col)
+        {
+            if (check(row, col))
+            {
+                queens[row] = col;
+                searchSolution(row + 1);
+            }
+        }
+    }
+    return;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    searchSolution(0);
+    cout << "Всего решений: " << counter << endl;
+    return 0;
+}
---
+++ Задача о восьми ферзях/8 ферзей/8 ферзей/8 ферзей.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 8; // Размер доски
+
+// Доска будет представлена двумерным массивом
+char board[N][N];
+
+// Функция для печати доски
+void printBoard() {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cout << board[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// Проверка безопасности размещения ферзя
+bool isSafe(int row, int col) {
+    // Проверка по вертикали
+    for (int i = 0; i < row; ++i) {
+        if (board[i][col] == 'Q') {
+            return false;
+        }
+    }
+
+    // Проверка по левой диагонали
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    // Проверка по правой диагонали
+    for (int i = row - 1, j = col + 1; i >= 0 && j < N; --i, ++j) {
+        if (board[i][j] == 'Q') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Рекурсивная функция для размещения ферзей
+bool solveNQueens(int row) {
+    if (row == N) {
+        printBoard();
+        return true;
+    }
+
+    bool result = false;
+    for (int col = 0; col < N && !result; ++col) {
+        if (isSafe(row, col)) {
+            board[row][col] = 'Q';
+            result = solveNQueens(row + 1);
+            if (!result) {
+                board[row][col] = '*'; // Сброс ячейки при неудаче
+            }
+            else {
+                break; // Решение найдено, прерываем цикл
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    // Инициализация доски звездочками
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            board[i][j] = '*';
+        }
+    }
+
+    if (solveNQueens(0)) {
+        cout << "Решение найдено!" << endl;
+    }
+    else {
+        cout << "Нет решений." << endl;
+    }
+
+    return 0;
+}
